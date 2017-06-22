51e171775853766ff7880a6ec9f081e0bc523bcc
jeddy016 <jeddy016@gmail.com>
2017-06-22 09:15:10 -0500

    Start coding login form validation

--- public/javascripts/controllers/loginController.js
+++ public/javascripts/controllers/loginController.js
@@ -1,22 +1,35 @@
-angular.module('pitStop').controller('loginController', ['$scope', '$location', function($scope, $location) {
+angular.module('pitStop').controller('loginController', ['$scope', '$location', '$http', function($scope, $location, $http) {
 
 	$scope.email= $scope.email;
 	$scope.password = $scope.password;
+
 	$scope.newPassword = $scope.newPassword;
-	$scope.email = $scope.email;
     $scope.newEmail = $scope.newEmail;
     $scope.confirmPassword= $scope.confirmPassword;
+
     $scope.loginVisible= false;
     $scope.error = "";
 
     $scope.loginShowHide= function() {
                 $scope.loginVisible= $scope.loginVisible ? false : true;
-            };
+    };
 
     $scope.validate = function () {
         if ($scope.email == "admin" && $scope.password == "admin")
         {
-            $scope.goTo('/home');
+            $http({
+                method: 'POST',
+                url: '/login',
+               //TODO: make sending data to java work
+                data: $scope.email,
+             })
+            .then(function(response) {
+                if(response.status == 200){
+                    console.log("Authenticated");
+                    $scope.goTo('/home');
+                };
+            })
+
         }
         else
         {
